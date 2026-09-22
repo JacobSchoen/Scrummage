ecd0cba4ed3d8defffd0e7c94b2a991239b02ed6
JacobSchoen <schoen.jacob@gmail.com>
2026-09-22 02:01:54 -0400

    setup env for supabase

---
+++ src/app/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://vgodhvffymlsvwzatagb.supabase.co',
+  supabasePublishableKey: 'sb_publishable_7bPZpVlKkxUfyNijheuGiQ_WgN5tgpP',
+};
